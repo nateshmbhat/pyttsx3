b49db7819755d406ec6c79edbdfc1f0de7ca0016
nateshmbhat <nateshmbhat1@gmail.com>
2017-06-25 01:24:07 +0530

    setup.py changed

--- setup.py
+++ setup.py
@@ -8,23 +8,8 @@ setup(
     description = '''
 An OFFLINE Python Text to Speech library (TTS) which works for both python3 and python2.
 This library very usefull especially if you don't want any delay in the speech produced and don't want to depend only on the internet for TTS conversion.
-It also supports multiple TTS engines like Sapi5 , nsss
+It also supports multiple TTS engines like Sapi5 , nsss , espeak .
 
-Installation :-
-
-pip install pyttsx3
-
-
-Usage :-
-
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("Hello its me . I am a talking computer.")
-engine.say("This is my second sentence")
-engine.runAndWait()
-
-
-complete usage docs can be found at :- 
 
 ''' ,
     summary = 'Offline Text to Speech library with multi-engine support'  ,

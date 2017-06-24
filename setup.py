3c4c71c68aaf6acb53f97fa50724df23cdd542d9
nateshmbhat <nateshmbhat1@gmail.com>
2017-06-25 02:14:15 +0530

    setup.py and MANIFEST changed

--- setup.py
+++ setup.py
@@ -21,24 +21,12 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.'''
 
 from distutils.core import setup
-import platform
-from setuptools import setup
-
-install_requires = []
-if platform.system() == 'Windows':
-    install_requires = [
-        'win32com'
-    ]
-elif platform.system() == 'Darwin':
-    install_requires = [
-        'pyobjc>=2.4'
-    ]
 
 setup(
 
     name='pyttsx3',
     packages = ['pyttsx3','pyttsx3.drivers'] ,
-    version = '1.13',
+    version = '1.15',
     description = '''
 An OFFLINE Python Text to Speech library (TTS) which works for both python3 and python2.
 This library very usefull especially if you don't want any delay in the speech produced and don't want to depend only on the internet for TTS conversion.
@@ -48,8 +36,7 @@ It also supports multiple TTS engines like Sapi5 , nsss , espeak .
     author = 'Natesh M Bhat' ,
     url = 'https://github.com/nateshmbhat/pyttsx3',
     author_email = 'nateshmbhatofficial@gmail.com' ,
-    download_url = 'https://github.com/nateshmbhat/pyttsx3/archive/v1.13.tar.gz',
+    download_url = 'https://github.com/nateshmbhat/pyttsx3/archive/v1.15.tar.gz',
     keywords=['ivona','pyttsx for python3' , 'TTS for python3' , 'pyttsx3' ,'text to speech for python','tts','text to speech','speech','speech synthesis','offline text to speech','offline tts','gtts'],
     classifiers = [] ,
-    install_requires=install_requires
 )

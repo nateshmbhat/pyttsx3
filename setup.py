0bc3fc4ed44f8e7d698cbeafc703b0fd40cf41ad
nateshmbhat <nateshmbhat1@gmail.com>
2017-06-25 01:47:06 +0530

    setup.py changed

--- setup.py
+++ setup.py
@@ -1,10 +1,32 @@
+'''MIT License
+
+Copyright (c) 2017 nateshmbhat
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.'''
+
 from distutils.core import setup
 
 setup(
 
     name='pyttsx3',
-    packages = ['pyttsx3'] ,
-    version = '1.12',
+    packages = ['pyttsx3','pyttsx3.drivers'] ,
+    version = '1.13',
     description = '''
 An OFFLINE Python Text to Speech library (TTS) which works for both python3 and python2.
 This library very usefull especially if you don't want any delay in the speech produced and don't want to depend only on the internet for TTS conversion.
@@ -14,7 +36,7 @@ It also supports multiple TTS engines like Sapi5 , nsss , espeak .
     author = 'Natesh M Bhat' ,
     url = 'https://github.com/nateshmbhat/pyttsx3',
     author_email = 'nateshmbhatofficial@gmail.com' ,
-    download_url = 'https://github.com/nateshmbhat/pyttsx3/archive/v1.12.tar.gz',
+    download_url = 'https://github.com/nateshmbhat/pyttsx3/archive/v1.13.tar.gz',
     keywords=['ivona','pyttsx for python3' , 'TTS for python3' , 'pyttsx3' ,'text to speech for python','tts','text to speech','speech','speech synthesis','offline text to speech','offline tts','gtts'],
     classifiers = [] ,
 

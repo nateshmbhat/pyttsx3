b895c608182ae734047378450556c2abd73da905
nateshmbhat <nateshmbhat1@gmail.com>
2017-06-25 01:28:03 +0530

    setup.py changed

--- setup.py
+++ setup.py
@@ -4,19 +4,17 @@ setup(
 
     name='pyttsx3',
     packages = ['pyttsx3'] ,
-    version = '1.1',
+    version = '1.12',
     description = '''
 An OFFLINE Python Text to Speech library (TTS) which works for both python3 and python2.
 This library very usefull especially if you don't want any delay in the speech produced and don't want to depend only on the internet for TTS conversion.
 It also supports multiple TTS engines like Sapi5 , nsss , espeak .
-
-
 ''' ,
     summary = 'Offline Text to Speech library with multi-engine support'  ,
     author = 'Natesh M Bhat' ,
     url = 'https://github.com/nateshmbhat/pyttsx3',
     author_email = 'nateshmbhatofficial@gmail.com' ,
-    download_url = 'https://github.com/nateshmbhat/pyttsx3/archive/v1.1.tar.gz',
+    download_url = 'https://github.com/nateshmbhat/pyttsx3/archive/v1.12.tar.gz',
     keywords=['ivona','pyttsx for python3' , 'TTS for python3' , 'pyttsx3' ,'text to speech for python','tts','text to speech','speech','speech synthesis','offline text to speech','offline tts','gtts'],
     classifiers = [] ,
 

b73c8711f8badd2b4e5b9419115ffc10b5592f11
nateshmbhat <nateshmbhat1@gmail.com>
2017-11-29 16:56:21 +0530

    updated setup.py and removed multipline description

--- setup.py
+++ setup.py
@@ -33,23 +33,23 @@ elif platform.system() == 'Darwin':
         'pyobjc>=2.4'
     ]
 
+with open('README.rst' , 'r') as f:
+    long_description = f.read() 
 
 setup(
 
     name='pyttsx3',
     packages = ['pyttsx3','pyttsx3.drivers'] ,
-    version = '2.6',
-    description = '''
-An OFFLINE Python Text to Speech library (TTS) which works for both python3 and python2.
-This library very usefull especially if you don't want any delay in the speech produced and don't want to depend only on the internet for TTS conversion.
-It also supports multiple TTS engines like Sapi5 , nsss , espeak .
+    version = '2.7',
+    description = '''An OFFLINE Python Text to Speech library (TTS) which works for both python3 and python2.This library very usefull especially if you don't want any delay in the speech produced and don't want to depend only on the internet for TTS conversion. It also supports multiple TTS engines like Sapi5 , nsss , espeak .
 ''' ,
+    long_description = long_description , 
     summary = 'Offline Text to Speech library with multi-engine support'  ,
     author = 'Natesh M Bhat' ,
     url = 'https://github.com/nateshmbhat/pyttsx3',
     author_email = 'nateshmbhatofficial@gmail.com' ,
-    download_url = 'https://github.com/nateshmbhat/pyttsx3/archive/v2.6.tar.gz',
-    keywords=['ivona','pyttsx for python3' , 'TTS for python3' , 'pyttsx3' ,'text to speech for python','tts','text to speech','speech','speech synthesis','offline text to speech','offline tts','gtts'],
+    #download_url = 'https://github.com/nateshmbhat/pyttsx3/archive/v2.6.tar.gz',
+    keywords=['pyttsx' , 'ivona','pyttsx for python3' , 'TTS for python3' , 'pyttsx3' ,'text to speech for python','tts','text to speech','speech','speech synthesis','offline text to speech','offline tts','gtts'],
     classifiers = [] ,
     install_requires=install_requires
 )

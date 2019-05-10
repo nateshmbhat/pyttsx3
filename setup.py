5a19376a94fdef6bfaef8795539e755b1f363fbf
nateshmbhat <nateshmbhat1@gmail.com>
2019-05-10 20:48:38 +0530

    fixed types and changed license to GPL3

--- setup.py
+++ setup.py
@@ -30,16 +30,28 @@ with open('README.rst', 'r') as f:
 setup(
     name='pyttsx3',
     packages=['pyttsx3', 'pyttsx3.drivers'],
-    version='2.7',
+    version='2.71',
     description='Text to Speech (TTS) library for Python 2 and 3. Works without internet connection or delay. Supports multiple TTS engines, including Sapi5, nsss, and espeak.',
     long_description=long_description,
     summary='Offline Text to Speech library with multi-engine support',
     author='Natesh M Bhat',
     url='https://github.com/nateshmbhat/pyttsx3',
     author_email='nateshmbhatofficial@gmail.com',
-    #download_url = 'https://github.com/nateshmbhat/pyttsx3/archive/v2.6.tar.gz',
-    install_requires=install_requires
+    install_requires=install_requires ,
     keywords=['pyttsx' , 'ivona','pyttsx for python3' , 'TTS for python3' , 'pyttsx3' ,'text to speech for python','tts','text to speech','speech','speech synthesis','offline text to speech','offline tts','gtts'],
-    classifiers = [] ,
+    classifiers = [
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: System Administrators',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7'
+    ],
     extras_require=extras_require
 )

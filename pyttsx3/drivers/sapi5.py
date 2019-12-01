nateshmbhat/pyttsx3

commit cbb3dc2b5dcfe4fa3cfc51d3d79e40228dddc07f

Implement Windows Support in save_to_file (#57)
* idk why but this is not working

* FIXED

* make an indicator that it is modified

* fix

* Implement a working save_to_file

* Cleanup code

* Typo

pyttsx3/drivers/sapi5.py
@@ -1,10 +1,16 @@
+import comtypes.client  # Importing comtypes.client will make the gen subpackage
+try:
+    from comtypes.gen import SpeechLib  # comtypes
+except ImportError:
+    # Generate the SpeechLib lib and any associated files
+    engine = comtypes.client.CreateObject("SAPI.SpVoice")
+    stream = comtypes.client.CreateObject("SAPI.SpFileStream")
+    from comtypes.gen import SpeechLib
 
-#import comtypes.client
-import win32com.client
 import pythoncom
 import time
 import math
-import weakref
+import os
 from ..voice import Voice
 from . import toUtf8, fromUtf8
 
@@ -25,15 +31,10 @@ def buildDriver(proxy):
 
 class SAPI5Driver(object):
     def __init__(self, proxy):
-        self._tts = win32com.client.Dispatch('SAPI.SPVoice')
-        #self._tts = comtypes.client.CreateObject('SAPI.SPVoice')
+        self._tts = comtypes.client.CreateObject('SAPI.SPVoice')
         # all events
         self._tts.EventInterests = 33790
-        self._advise = win32com.client.WithEvents(self._tts,
-                                                  SAPI5DriverEventSink)
-        self._advise.setDriver(weakref.proxy(self))
-        #self._debug = comtypes.client.ShowEvents(self._tts)
-        #self._advise = comtypes.client.GetEvents(self._tts, self)
+        self._advise = comtypes.client.GetEvents(self._tts, self)
         self._proxy = proxy
         self._looping = False
         self._speaking = False
@@ -59,7 +60,16 @@ class SAPI5Driver(object):
         self._tts.Speak('', 3)
 
     def save_to_file(self, text, filename):
-        raise NotImplementedError
+        cwd = os.getcwd()
+        stream = comtypes.client.CreateObject('SAPI.SPFileStream')
+        stream.Open(filename, SpeechLib.SSFMCreateForWrite)
+
+        temp_stream = self._tts.AudioOutputStream
+        self._tts.AudioOutputStream = stream
+        self._tts.Speak(text)
+        self._tts.AudioOutputStream = temp_stream
+        stream.close()
+        os.chdir(cwd)
 
     def _toVoice(self, attr):
         return Voice(attr.Id, attr.GetDescription())

setup.py
@@ -7,7 +7,7 @@ extras_require = {
         'pyobjc>=2.4',
     ],
     ':"win32" in sys_platform': [
-        'pypiwin32',
+        'comtypes'
     ],
 }
 

commit 021a97bea88fee471fc179b4119fe5cc06566185

Fix a bug introduced in #57 (#75)
* idk why but this is not working

* FIXED

* make an indicator that it is modified

* fix

* Implement a working save_to_file

* Cleanup code

* Typo

* Fix bug: runandwait runs indefinitely after say

pyttsx3/drivers/sapi5.py
@@ -11,6 +11,7 @@ import pythoncom
 import time
 import math
 import os
+import weakref
 from ..voice import Voice
 from . import toUtf8, fromUtf8
 
@@ -34,7 +35,9 @@ class SAPI5Driver(object):
         self._tts = comtypes.client.CreateObject('SAPI.SPVoice')
         # all events
         self._tts.EventInterests = 33790
-        self._advise = comtypes.client.GetEvents(self._tts, self)
+        self._event_sink = SAPI5DriverEventSink()
+        self._event_sink.setDriver(weakref.proxy(self))
+        self._advise = comtypes.client.GetEvents(self._tts, self._event_sink)
         self._proxy = proxy
         self._looping = False
         self._speaking = False
@@ -50,7 +53,7 @@ class SAPI5Driver(object):
         self._proxy.setBusy(True)
         self._proxy.notify('started-utterance')
         self._speaking = True
-        self._tts.Speak(fromUtf8(toUtf8(text)), 19)
+        self._tts.Speak(fromUtf8(toUtf8(text)))
 
     def stop(self):
         if not self._speaking:
@@ -63,10 +66,9 @@ class SAPI5Driver(object):
         cwd = os.getcwd()
         stream = comtypes.client.CreateObject('SAPI.SPFileStream')
         stream.Open(filename, SpeechLib.SSFMCreateForWrite)
-
         temp_stream = self._tts.AudioOutputStream
         self._tts.AudioOutputStream = stream
-        self._tts.Speak(text)
+        self._tts.Speak(fromUtf8(toUtf8(text)))
         self._tts.AudioOutputStream = temp_stream
         stream.close()
         os.chdir(cwd)
@@ -142,11 +144,11 @@ class SAPI5DriverEventSink(object):
     def setDriver(self, driver):
         self._driver = driver
 
-    def OnWord(self, stream, pos, char, length):
+    def _ISpeechVoiceEvents_StartStream(self, char, length):
         self._driver._proxy.notify(
             'started-word', location=char, length=length)
 
-    def OnEndStream(self, stream, pos):
+    def _ISpeechVoiceEvents_EndStream(self, stream, pos):
         d = self._driver
         if d._speaking:
             d._proxy.notify('finished-utterance', completed=not d._stopping)
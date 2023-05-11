4c210745ed92dd48d251764c33636f31b65e4610
Jiangshan00001 <710806594@qq.com>
2023-05-11 22:59:52 +0800

    test macos using github actions

--- pyttsx4/drivers/coqui_ai_tts.py
+++ pyttsx4/drivers/coqui_ai_tts.py
@@ -1,5 +1,5 @@
 #coding:utf-8
-
+import time
 try:
     from TTS.api import TTS
 except ImportError:
@@ -17,9 +17,6 @@ class TTSDriver(object):
         self.model_name = TTS.list_models()[0]
         self._tts = TTS(self.model_name)
 
-        # all events
-        self._tts.EventInterests = 33790
-
         self._proxy = proxy
         self._looping = False
         self._speaking = False
@@ -30,57 +27,25 @@ class TTSDriver(object):
         self._tts=None
 
     def say(self, text):
-        self._proxy.setBusy(True)
-        self._proxy.notify('started-utterance')
-        self._speaking = True
-        self._tts.Speak(self.pitch_str+fromUtf8(toUtf8(text)))
+        # self._proxy.setBusy(True)
+        # self._proxy.notify('started-utterance')
+        # self._speaking = True
+        # self._tts.Speak(self.pitch_str+fromUtf8(toUtf8(text)))
+        # TBD
+        pass
 
     def stop(self):
-        if not self._speaking:
-            return
-        self._proxy.setBusy(True)
-        self._stopping = True
-        self._tts.Speak('', 3)
+        # if not self._speaking:
+        #     return
+        # self._proxy.setBusy(True)
+        # self._stopping = True
+        # self._tts.Speak('', 3)
+        pass
 
     def save_to_file(self, text, filename):
-        if isinstance(filename, BytesIO):
-            self.to_memory(text, filename)
-            return
+        self._tts.tts_to_file(text, filename)
+        
 
-        cwd = os.getcwd()
-        stream = comtypes.client.CreateObject('SAPI.SPFileStream')
-        stream.Open(filename, SpeechLib.SSFMCreateForWrite)
-
-        # in case there is no outputstream, the call to AudioOutputStream will fail
-        is_stream_stored = False
-        try:
-            temp_stream = self._tts.AudioOutputStream
-            is_stream_stored=True
-        except Exception as e:
-            #no audio output stream
-            pass
-        self._tts.AudioOutputStream = stream
-        self._tts.Speak(self.pitch_str+fromUtf8(toUtf8(text)))
-        if is_stream_stored:
-            self._tts.AudioOutputStream = temp_stream
-        else:
-            try:
-                self._tts.AudioOutputStream = None
-            except Exception as e:
-                print('set None no no-output stream machine:', e)
-                pass
-        stream.close()
-        os.chdir(cwd)
-
-    def to_memory(self, text, olist):
-        stream = comtypes.client.CreateObject('SAPI.SpMemoryStream')
-        temp_stream = self._tts.AudioOutputStream
-        self._tts.AudioOutputStream = stream
-        self._tts.Speak(self.pitch_str+fromUtf8(toUtf8(text)))
-        self._tts.AudioOutputStream = temp_stream
-        data = stream.GetData()
-        olist.write(bytes(data))
-        del stream
 
     def _toVoice(self, attr):
         return Voice(attr.Id, attr.GetDescription())
@@ -94,7 +59,7 @@ class TTSDriver(object):
 
     def getProperty(self, name):
         if name == 'voices':
-            return [self._toVoice(attr) for attr in self._tts.GetVoices()]
+            return TTS.list_models()
         elif name == 'voice':
             return self._tts.Voice.Id
         elif name == 'rate':
@@ -145,7 +110,6 @@ class TTSDriver(object):
             if first:
                 self._proxy.setBusy(False)
                 first = False
-            pythoncom.PumpWaitingMessages()
             time.sleep(0.05)
 
     def endLoop(self):
@@ -154,25 +118,4 @@ class TTSDriver(object):
     def iterate(self):
         self._proxy.setBusy(False)
         while 1:
-            pythoncom.PumpWaitingMessages()
             yield
-
-
-class SAPI5DriverEventSink(object):
-    def __init__(self):
-        self._driver = None
-
-    def setDriver(self, driver):
-        self._driver = driver
-
-    def _ISpeechVoiceEvents_StartStream(self, char, length):
-        self._driver._proxy.notify(
-            'started-word', location=char, length=length)
-
-    def _ISpeechVoiceEvents_EndStream(self, stream, pos):
-        d = self._driver
-        if d._speaking:
-            d._proxy.notify('finished-utterance', completed=not d._stopping)
-        d._speaking = False
-        d._stopping = False
-        d._proxy.setBusy(False)
---
+++ pyttsx4_test.py
@@ -0,0 +1,113 @@
+# coding:utf-8
+
+from io import BytesIO
+import time
+
+import pyttsx4
+
+import os
+
+
+def test_1():
+    # engine = pyttsx4.init('coqui_ai_tts') # object creation
+    engine = pyttsx4.init()
+
+    ############
+    engine.setProperty('pitch', -20)
+
+    while True:
+        # engine.save_to_file('Hello World', 'test.wav')
+        engine.say('Hello world')
+        engine.runAndWait()
+        time.sleep(4)
+
+
+def test_2():
+    engine = pyttsx4.init()
+
+    engine.setProperty('pitch', 0)
+    # engine.save_to_file('Hello World', 'test.wav')
+    engine.say('你好,我是一个程序员')
+    engine.runAndWait()
+
+    engine.setProperty('pitch', 10)
+    # engine.save_to_file('Hello World', 'test.wav')
+    engine.say('你好,我是一个程序员')
+    engine.runAndWait()
+
+    engine.setProperty('pitch', 50)
+    # engine.save_to_file('Hello World', 'test.wav')
+    engine.say('你好,我是一个程序员')
+    engine.runAndWait()
+
+
+def test_3():
+
+    engine = pyttsx4.init()
+
+    b = BytesIO()
+    engine.save_to_file('Hello World', b)
+    engine.runAndWait()
+
+    bs = b.getvalue()
+    # to save b as a wav file, we have to add a wav file header:44byte
+    # https://docs.fileformat.com/audio/wav/
+    # b= bytes(b'RIFF')+ (len(bs)+38).to_bytes(4, byteorder='little')+b'WAVEfmt\x20' +\
+    #   (18).to_bytes(4, byteorder='little')+(1).to_bytes(2, byteorder='little') +(1).to_bytes(2, byteorder='little')+ \
+    #   (22050).to_bytes(4,byteorder='little')+(44100).to_bytes(4,byteorder='little') +(2).to_bytes(2, byteorder='little')+\
+    #   (16).to_bytes(2,byteorder='little')+b'\x00\x00'+  b'data'+ (len(bs)).to_bytes(4, byteorder='little')+bs
+
+    b = bytes(b'RIFF') + (len(bs)+38).to_bytes(4, byteorder='little')+b'WAVEfmt\x20\x12\x00\x00' \
+        b'\x00\x01\x00\x01\x00' \
+        b'\x22\x56\x00\x00\x44\xac\x00\x00' +\
+        b'\x02\x00\x10\x00\x00\x00data' + \
+        (len(bs)).to_bytes(4, byteorder='little')+bs
+
+    f = open('test4.wav', 'wb')
+    f.write(b)
+    f.close()
+
+    engine.setProperty(
+        'voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
+
+    engine.say('Hello World')
+    engine.say('你好，世界')
+    engine.runAndWait()
+
+    """ RATE"""
+    rate = engine.getProperty(
+        'rate')   # getting details of current speaking rate
+    print(rate)  # printing current voice rate
+    engine.setProperty('rate', 125)     # setting up new voice rate
+
+    """VOLUME"""
+    volume = engine.getProperty(
+        'volume')  # getting to know current volume level (min=0 and max=1)
+    print(volume)  # printing current volume level
+    # setting up volume level  between 0 and 1
+    engine.setProperty('volume', 1.0)
+
+    """VOICE"""
+    voices = engine.getProperty('voices')  # getting details of current voice
+    # engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+    # changing index, changes voices. 1 for female
+    engine.setProperty('voice', voices[1].id)
+
+    """PITCH"""
+    pitch = engine.getProperty('pitch')  # Get current pitch value
+    print(pitch)  # Print current pitch value
+    # Set the pitch (default 50) to 75 out of 100
+    engine.setProperty('pitch', 75)
+
+    engine.say("Hello World!")
+    engine.say('My current speaking rate is ' + str(rate))
+    engine.runAndWait()
+    engine.stop()
+
+    """Saving Voice to a file"""
+    # On linux make sure that 'espeak' and 'ffmpeg' are installed
+    engine.save_to_file('Hello World', 'test.mp3')
+    engine.runAndWait()
+
+    if __name__ == '__main__':
+        test_1()

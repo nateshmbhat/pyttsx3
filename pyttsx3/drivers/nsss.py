41137e90890248288fb4c740a5b86e54c397c004
will wade <willwade@gmail.com>
2024-09-10 05:21:41 +0100

    many fixes for word event timings in nsss and sapi WIP (#322)

--- pyttsx3/drivers/nsss.py
+++ pyttsx3/drivers/nsss.py
@@ -1,33 +1,50 @@
-
-from Foundation import *
+# noinspection PyUnresolvedReferences
+import objc
 from AppKit import NSSpeechSynthesizer
+from Foundation import *
 from PyObjCTools import AppHelper
+# noinspection PyProtectedMember
 from PyObjCTools.AppHelper import PyObjCAppHelperRunLoopStopper
+
 from ..voice import Voice
 
+
+# noinspection PyUnresolvedReferences
 class RunLoopStopper(PyObjCAppHelperRunLoopStopper):
+    """
+    Overrides PyObjCAppHelperRunLoopStopper to terminate after endLoop.
+    """
+
+    def __init__(self):
+        self.shouldStop = False
 
     def init(self):
-        self = super(RunLoopStopper, self).init()
-        self.shouldStop = False
-        return self
+        return objc.super(RunLoopStopper, self).init()
 
     def stop(self):
         self.shouldStop = True
-        # this should go away when/if runEventLoop uses
-        # runLoop iteration
-        # if NSApp() is not None:
-        #     NSApp().terminate_(self)
 
+
+# noinspection PyPep8Naming
 def buildDriver(proxy):
     return NSSpeechDriver.alloc().initWithProxy(proxy)
 
 
+# noinspection PyUnresolvedReferences,PyPep8Naming,PyUnusedLocal
 class NSSpeechDriver(NSObject):
+
+    def __init__(self):
+        self._proxy = None
+        self._tts = None
+        self._completed = False
+
     @objc.python_method
     def initWithProxy(self, proxy):
-        self = super(NSSpeechDriver, self).init()
-        if self:
+        try:
+            proxy_attr = objc.super(NSSpeechDriver, self).init()
+        except AttributeError:
+            proxy_attr = self
+        if proxy_attr:
             self._proxy = proxy
             self._tts = NSSpeechSynthesizer.alloc().initWithVoice_(None)
             self._tts.setDelegate_(self)
@@ -44,24 +61,25 @@ class NSSpeechDriver(NSObject):
         self._proxy.setBusy(False)
 
     def startLoop(self):
+        # https://github.com/ronaldoussoren/pyobjc/blob/mater/pyobjc-framework-Cocoa/Lib/PyObjCTools/AppHelper.py#L243C25-L243C25  # noqa
         NSTimer.scheduledTimerWithTimeInterval_target_selector_userInfo_repeats_(
-            0.0, self, 'onPumpFirst:', None, False)
+            0.0, self, 'onPumpFirst:', None, False
+        )
         runLoop = NSRunLoop.currentRunLoop()
         stopper = RunLoopStopper.alloc().init()
         PyObjCAppHelperRunLoopStopper.addRunLoopStopper_toRunLoop_(stopper, runLoop)
-        try:
+        while stopper.shouldRun():
+            nextfire = runLoop.limitDateForMode_(NSDefaultRunLoopMode)
+            soon = NSDate.dateWithTimeIntervalSinceNow_(0)  # maxTimeout in runConsoleEventLoop
+            if nextfire is not None:
+                nextfire = soon.earlierDate_(nextfire)
+            if not runLoop.runMode_beforeDate_(NSDefaultRunLoopMode, nextfire):
+                stopper.stop()
+                break
+        PyObjCAppHelperRunLoopStopper.removeRunLoopStopperFromRunLoop_(runLoop)
 
-            while stopper.shouldRun():
-                nextfire = runLoop.limitDateForMode_(NSDefaultRunLoopMode)
-                if not stopper.shouldRun():
-                    break
-                if not runLoop.runMode_beforeDate_(NSDefaultRunLoopMode, nextfire):
-                    stopper.stop()
-
-        finally:
-            PyObjCAppHelperRunLoopStopper.removeRunLoopStopperFromRunLoop_(runLoop)
-
-    def endLoop(self):
+    @staticmethod
+    def endLoop():
         AppHelper.stopEventLoop()
 
     def iterate(self):
@@ -82,12 +100,9 @@ class NSSpeechDriver(NSObject):
 
     @objc.python_method
     def _toVoice(self, attr):
-        try:
-            lang = attr['VoiceLocaleIdentifier']
-        except KeyError:
-            lang = attr['VoiceLanguage']
-        return Voice(attr['VoiceIdentifier'], attr['VoiceName'],
-                     [lang], attr['VoiceGender'])
+        return Voice(attr.get('VoiceIdentifier'), attr.get('VoiceName'),
+                     [attr.get('VoiceLocaleIdentifier', attr.get('VoiceLanguage'))], attr.get('VoiceGender'),
+                     attr.get('VoiceAge'))
 
     @objc.python_method
     def getProperty(self, name):
@@ -125,11 +140,10 @@ class NSSpeechDriver(NSObject):
 
     @objc.python_method
     def save_to_file(self, text, filename):
+        self._proxy.setBusy(True)
+        self._completed = True
         url = Foundation.NSURL.fileURLWithPath_(filename)
-        import time
-
         self._tts.startSpeakingString_toURL_(text, url)
-        time.sleep(max(1, len(text) * 0.01))
 
     def speechSynthesizer_didFinishSpeaking_(self, tts, success):
         if not self._completed:

29f10c819577af56fa8cd77e2613025a4dad29bf
will wade <willwade@gmail.com>
2024-09-14 16:09:46 +0100

    Fix docs (#321)
    * small tweaks to docs
    
    docs are using old py2 code  which is super annoying.
    
    * pyttsx3 not py3-tts

--- example/main.py
+++ example/main.py
@@ -3,13 +3,13 @@ engine = pyttsx3.init() # object creation
 
 """ RATE"""
 rate = engine.getProperty('rate')   # getting details of current speaking rate
-print (rate)                        #printing current voice rate
+print(rate)                        #printing current voice rate
 engine.setProperty('rate', 125)     # setting up new voice rate
 
 
 """VOLUME"""
 volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
-print (volume)                          #printing current volume level
+print(volume)                          #printing current volume level
 engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
 
 """VOICE"""

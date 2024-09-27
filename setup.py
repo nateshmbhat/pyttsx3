996cd361a2d30c9cca0db7b6879f3ac2c27766e1
Natesh Bhat <nateshmbhat1@gmail.com>
2024-09-27 06:30:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.rst', 'r') as f:
 setup(
     name='pyttsx3',
     packages=['pyttsx3', 'pyttsx3.drivers'],
-    version='2.97',
+    version='2.98',
     description='Text to Speech (TTS) library for Python 3. Works without internet connection or delay. Supports multiple TTS engines, including Sapi5, nsss, and espeak.',
     long_description=long_description,
     summary='Offline Text to Speech library with multi-engine support',

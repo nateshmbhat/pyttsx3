7d673f3a8f455ff202a2bfe98764eb24f058f0ba
will wade <willwade@gmail.com>
2024-10-27 21:40:59 +0000

    updating engine to check hasattr queue in setBusy
    hard to see with black changes but basically " if not self._busy and hasattr(self, "_queue"):" in setBusy
    
    cant see why this would break other engines.. famous last words

--- pyttsx3/driver.py
+++ pyttsx3/driver.py
@@ -5,7 +5,7 @@ import importlib
 
 
 class DriverProxy(object):
-    '''
+    """
     Proxy to a driver implementation.
 
     @ivar _module: Module containing the driver implementation
@@ -23,10 +23,10 @@ class DriverProxy(object):
     @type _debug: bool
     @ivar _iterator: Driver iterator to invoke when in an external run loop
     @type _iterator: iterator
-    '''
+    """
 
     def __init__(self, engine, driverName, debug):
-        '''
+        """
         Constructor.
 
         @param engine: Reference to the engine that owns the driver
@@ -36,17 +36,17 @@ class DriverProxy(object):
         @type driverName: str
         @param debug: Debugging output enabled or not
         @type debug: bool
-        '''
+        """
         if driverName is None:
             # pick default driver for common platforms
-            if sys.platform == 'darwin':
-                driverName = 'nsss'
-            elif sys.platform == 'win32':
-                driverName = 'sapi5'
+            if sys.platform == "darwin":
+                driverName = "nsss"
+            elif sys.platform == "win32":
+                driverName = "sapi5"
             else:
-                driverName = 'espeak'
+                driverName = "espeak"
         # import driver module
-        name = 'pyttsx3.drivers.%s' % driverName
+        name = "pyttsx3.drivers.%s" % driverName
         self._module = importlib.import_module(name)
         # build driver instance
         self._driver = self._module.buildDriver(weakref.proxy(self))
@@ -57,7 +57,7 @@ class DriverProxy(object):
         self._name = None
         self._iterator = None
         self._debug = debug
-        self._current_text = ''
+        self._current_text = ""
 
     def __del__(self):
         try:
@@ -66,7 +66,7 @@ class DriverProxy(object):
             pass
 
     def _push(self, mtd, args, name=None):
-        '''
+        """
         Adds a command to the queue.
 
         @param mtd: Method to invoke to process the command
@@ -75,138 +75,144 @@ class DriverProxy(object):
         @type args: tuple
         @param name: Name associated with the command
         @type name: str
-        '''
+        """
+        print(
+            f"[DEBUG] Adding command to queue: {mtd.__name__}, args: {args}, name: {name}"
+        )
         self._queue.append((mtd, args, name))
         self._pump()
 
     def _pump(self):
-        '''
+        """
         Attempts to process the next command in the queue if one exists and the
         driver is not currently busy.
-        '''
+        """
+        print(
+            f"[DEBUG] Pump called. Queue length: {len(self._queue)}, Busy status: {self._busy}"
+        )
         while (not self._busy) and len(self._queue):
             cmd = self._queue.pop(0)
             self._name = cmd[2]
             try:
                 cmd[0](*cmd[1])
             except Exception as e:
-                self.notify('error', exception=e)
+                self.notify("error", exception=e)
                 if self._debug:
                     traceback.print_exc()
 
     def notify(self, topic, **kwargs):
-        '''
+        """
         Sends a notification to the engine from the driver.
 
         @param topic: Notification topic
         @type topic: str
         @param kwargs: Arbitrary keyword arguments
         @type kwargs: dict
-        '''
-        if 'name' not in kwargs or kwargs['name'] is None:  # Avoid overwriting
-            kwargs['name'] = self._name
+        """
+        if "name" not in kwargs or kwargs["name"] is None:  # Avoid overwriting
+            kwargs["name"] = self._name
         self._engine._notify(topic, **kwargs)
 
     def setBusy(self, busy):
-        '''
+        """
         Called by the driver to indicate it is busy.
 
         @param busy: True when busy, false when idle
         @type busy: bool
-        '''
+        """
         self._busy = busy
-        if not self._busy:
+        if not self._busy and hasattr(self, "_queue"):
             self._pump()
 
     def isBusy(self):
-        '''
+        """
         @return: True if the driver is busy, false if not
         @rtype: bool
-        '''
+        """
         return self._busy
 
     def say(self, text, name):
-        '''
+        """
         Called by the engine to push a say command onto the queue.
 
         @param text: Text to speak
         @type text: unicode
         @param name: Name to associate with the utterance
         @type name: str
-        '''
+        """
         self._current_text = text
         self._push(self._driver.say, (text,), name)
 
     def stop(self):
-        '''
+        """
         Called by the engine to stop the current utterance and clear the queue
         of commands.
-        '''
+        """
         # clear queue up to first end loop command
-        while(True):
+        while True:
             try:
                 mtd, args, name = self._queue[0]
             except IndexError:
                 break
-            if(mtd == self._engine.endLoop):
+            if mtd == self._engine.endLoop:
                 break
             self._queue.pop(0)
         self._driver.stop()
 
     def save_to_file(self, text, filename, name):
-        '''
+        """
         Called by the engine to push a say command onto the queue.
 
         @param text: Text to speak
         @type text: unicode
         @param name: Name to associate with the utterance
         @type name: str
-        '''
+        """
         self._push(self._driver.save_to_file, (text, filename), name)
 
     def getProperty(self, name):
-        '''
+        """
         Called by the engine to get a driver property value.
 
         @param name: Name of the property
         @type name: str
         @return: Property value
         @rtype: object
-        '''
+        """
         return self._driver.getProperty(name)
 
     def setProperty(self, name, value):
-        '''
+        """
         Called by the engine to set a driver property value.
 
         @param name: Name of the property
         @type name: str
         @param value: Property value
         @type value: object
-        '''
+        """
         self._push(self._driver.setProperty, (name, value))
 
     def runAndWait(self):
-        '''
+        """
         Called by the engine to start an event loop, process all commands in
         the queue at the start of the loop, and then exit the loop.
-        '''
+        """
         self._push(self._engine.endLoop, tuple())
         self._driver.startLoop()
 
     def startLoop(self, useDriverLoop):
-        '''
+        """
         Called by the engine to start an event loop.
-        '''
+        """
         if useDriverLoop:
             self._driver.startLoop()
         else:
             self._iterator = self._driver.iterate()
 
     def endLoop(self, useDriverLoop):
-        '''
+        """
         Called by the engine to stop an event loop.
-        '''
+        """
         self._queue = []
         self._driver.stop()
         if useDriverLoop:
@@ -216,10 +222,10 @@ class DriverProxy(object):
         self.setBusy(True)
 
     def iterate(self):
-        '''
+        """
         Called by the engine to iterate driver commands and notifications from
         within an external event loop.
-        '''
+        """
         try:
             next(self._iterator)
         except StopIteration:

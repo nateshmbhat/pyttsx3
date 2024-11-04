7cecb089401e1502f6cd6bf468e5c6da0e5d674b
Christian Clauss <cclauss@me.com>
2024-11-04 16:02:55 +0100

    DRAFT: Add tests/test_engines.py

---
+++ tests/test_engines.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import pytest
+
+import pyttsx3
+
+quick_brown_fox = "The quick brown fox jumped over the lazy dog."
+
+
+@pytest.mark.parametrize("driver_name", pyttsx3.engine.engines_by_sys_platform())
+def test_engine_name(driver_name):
+    engine = pyttsx3.init(driver_name)
+    assert engine.driver_name == driver_name
+    assert str(engine) == driver_name
+    assert repr(engine) == f"pyttsx3.engine.Engine('{driver_name}', debug=False)"
+    engine.stop()
+
+
+@pytest.mark.parametrize("driver_name", pyttsx3.engine.engines_by_sys_platform())
+def test_speaking_text(driver_name):
+    engine = pyttsx3.init(driver_name)
+    engine.say("Sally sells seashells by the seashore.")
+    engine.say(quick_brown_fox)
+    print(list(pyttsx3._activeEngines.values()), flush=True)
+    engine.runAndWait()
+    print(list(pyttsx3._activeEngines.values()), flush=True)
+    engine.stop()

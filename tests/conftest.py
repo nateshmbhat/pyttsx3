49f9883503c179539e96a63cd9cb5cd065ee9f55
will wade <willwade@gmail.com>
2024-11-13 15:50:19 +0000

    reformat

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 
+
 def pytest_collection_modifyitems(items):
     """Skip tests after `test_changing_volume` if in CI environment."""
     if "CI" not in os.environ:
@@ -11,4 +12,8 @@ def pytest_collection_modifyitems(items):
         if "test_changing_volume" in item.nodeid:
             skip = True
         elif skip:
-            item.add_marker(pytest.mark.skip(reason="Skipping in CI environment after test_changing_volume"))
+            item.add_marker(
+                pytest.mark.skip(
+                    reason="Skipping in CI environment after test_changing_volume"
+                )
+            )

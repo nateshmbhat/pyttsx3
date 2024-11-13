abdddc53b6322c0f8f95846e49d2d9e6823d96eb
will wade <willwade@gmail.com>
2024-11-13 12:05:29 +0000

    Update tests/test_pyttsx3.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- tests/test_pyttsx3.py
+++ tests/test_pyttsx3.py
@@ -39,7 +39,7 @@ def test_speaking_text(engine):
 @pytest.mark.skipif(
     sys.platform not in ("darwin", "ios"), reason="Testing only on macOS and iOS"
 )
-def test_apple_avsynth_voices(engine):
+def test_apple_avspeech_voices(engine):
     import platform
 
     macos_version, _, macos_hardware = platform.mac_ver()
